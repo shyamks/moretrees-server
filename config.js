b83229389979d0c7b90086072857c2b89e8f09c8
Shyam Kodmad <shyam.kodmad@gmail.com>
2019-10-21 16:55:14 +0530

    useUnifiedTopology added

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ mongoose.Promise = global.Promise;
 
 const url = process.env.MONGODB_URL;
 
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
