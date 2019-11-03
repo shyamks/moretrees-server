e013e6beb971761e8ace1526ef026e8d83d9899d
Shyam Kodmad <shyam.kodmad@gmail.com>
2019-11-03 23:46:52 +0530

    background job

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ git pull
 
 for pid in `ps aux | grep [s]erver.ts | awk '{print $2}'` ; do kill -9 $pid ; done
 
-echo "After kill command, ts-node processes => "
+printf "After kill command, ts-node processes => "
 ps aux | grep '[s]erver.ts'
 
-npm run start:prod
+npm run start:prod &
